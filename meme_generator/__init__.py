a4acb9726da5e1e34e2a38b6a8ed95af9e7c8aaf
Juan40K <juan.lopez.de.la.plaza@alumnojoyfe.iepgroup.es>
2023-05-08 12:31:38 +0200

    deberes echos

---
+++ meme_generator/__init__.py
@@ -0,0 +1,53 @@
+import re
+import requests
+import textwrap
+
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+
+
+def generate_meme(src_path, save_path, top_text, bottom_text='', font_path='./fonts/impact/impact.ttf', font_size=9,
+                  stroke_width=5):
+
+    # load image
+    if re.match('https?://.+', src_path):
+        response = requests.get(src_path)
+        im = Image.open(BytesIO(response.content))
+    else:
+        im = Image.open(src_path)
+        
+    draw = ImageDraw.Draw(im)
+    image_width, image_height = im.size
+
+    # load font
+    font = ImageFont.truetype(font=font_path, size=int(image_height * font_size) // 100)
+
+    # convert text to uppercase
+    top_text = top_text.upper()
+    bottom_text = bottom_text.upper()
+
+    # text wrapping
+    char_width, char_height = font.getsize('A')
+    chars_per_line = image_width // char_width
+    top_lines = textwrap.wrap(top_text, width=chars_per_line)
+    bottom_lines = textwrap.wrap(bottom_text, width=chars_per_line)
+
+    # draw top lines
+    y = 10
+    for line in top_lines:
+        line_width, line_height = font.getsize(line)
+        x = (image_width - line_width) / 2
+        draw.text((x, y), line, fill='white', font=font, stroke_width=stroke_width, stroke_fill='black')
+        y += line_height
+
+    # draw bottom lines
+    y = image_height - char_height * len(bottom_lines) - 15
+    for line in bottom_lines:
+        line_width, line_height = font.getsize(line)
+        x = (image_width - line_width) / 2
+        draw.text((x, y), line, fill='white', font=font, stroke_width=stroke_width, stroke_fill='black')
+        y += line_height
+
+    # save meme
+    im.save(save_path)
+    
